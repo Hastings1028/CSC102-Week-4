d17a952b4d90b79d02d6dfbc80438ca17421febc
zack <96461074+Hastings1028@users.noreply.github.com>
2022-02-06 04:47:04 -0600

    Create buttons.js

---
+++ buttons.js
@@ -0,0 +1,14 @@
+// tells the html to read data  
+function start() {
+    document.getElementById("startButton").disabled = true;
+    document.getElementById("stopButton").disabled = false;
+    document.getElementById("data").rows["seconds"].innerHTML = "Reading Data!!";
+    document.getElementById("data").rows["longitude"].innerHTML = "Start Activated";
+}
+// telling the html to stop
+function stop() {
+    document.getElementById("data").rows["seconds"].innerHTML = "<td>Time Elapsed:</td><td>15 seconds</td>";
+    document.getElementById("data").rows["longitude"].innerHTML = "<td>Longitude:</td><td>0</td>";
+    document.getElementById("startButton").disabled = false;
+    document.getElementById("stopButton").disabled = true;
+}
